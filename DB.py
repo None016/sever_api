58a3e55ed14603c4fad00e677c9b746bba450953
LAPTOP-88JQ3VC3\1 <Hiro0161@yandex.ru>
2025-02-05 16:38:53 +0500

    первый

---
+++ DB.py
@@ -0,0 +1,57 @@
+import psycopg2
+from psycopg2.extras import execute_values
+
+
+class DB:
+    def __init__(self, data_base):
+        self.conn = psycopg2.connect(host='localhost',
+                                     database=data_base,
+                                     user='postgres',
+                                     password='hiro12')
+
+        self.cur = self.conn.cursor()
+
+    def select_by(self, param, data):
+        self.cur.execute(f"SELECT * FROM public.user WHERE {param} = %s", (data,))
+        return self.cur.fetchone()
+
+    def get_users(self):
+        self.cur.execute("SELECT * FROM public.user")
+        print(self.cur.fetchall())
+
+    def get_user_id(self, email):
+        answer = self.select_by("email", email)
+        if answer is not None:
+            return answer[0]
+        else:
+            return None
+
+    def set_user(self, name, email, password):
+        execute_values(self.cur, "INSERT INTO public.user(name, email, password) VALUES %s", [(name, email, password)])
+        self.conn.commit()
+
+    def check_is_reg(self, email):
+        self.cur.execute(
+            "SELECT EXISTS (SELECT * FROM public.user WHERE email = %s)",
+            (email,)
+        )
+        return self.cur.fetchall()[0][0]
+
+    def check_is_aut(self, email, password):
+        return self.select_by("email", email)[-1] == password
+
+
+    def __del__(self):
+        self.conn.close()
+        self.cur.close()
+
+
+if __name__ == "__main__":
+    db = DB("react_db")
+    print(db.check_is_aut("email@email.com", "11111111"))
+    print(db.select_by("id", 1))
+    # print(db.get_user_id("email@email.co"))
+    # db.get_users()
+
+
+
---
+++ main.py
@@ -0,0 +1,84 @@
+import datetime
+import secrets
+import time
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import jwt
+from pyexpat.errors import messages
+
+from DB import DB
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "qwer1234"
+CORS(app)
+
+
+@app.route('/reg', methods=['POST', 'GET']) # Убедитесь, что метод POST разрешен
+def reg():
+    if request.is_json:
+        try:
+            data = request.get_json()
+            print(f"Полученные данные: {data}")
+            db = DB("react_db")
+            print(db.check_is_reg(data["email"]))
+            if db.check_is_reg(data["email"]):
+                return jsonify({"message": "пользователь уже зарегестрирован"}), 409
+            else:
+                db.set_user(data["name"], data["email"], data["password"])
+                return jsonify({"message": "пользователь зарегестрирован"}), 200
+        except:
+            return jsonify({"messages": "Неверный формат данных"}), 415
+
+  
+@app.route("/aut", methods=["POST"])
+def autorization():
+    if request.is_json:
+        try:
+            db = DB("react_db")
+            data = request.get_json()
+            print(data)
+            if db.check_is_aut(data["email"], data["password"]):
+                print("___________________________")
+                print(datetime.datetime.utcnow())
+                token = jwt.encode({"id": db.select_by("email", data["email"])[0],
+                                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=3)}, app.config['SECRET_KEY'])
+                return jsonify({"message": "пользователь авторизован",
+                                "token": token}), 200
+            else:
+                return jsonify({"message": "пользоваетель не авторизован"}), 409
+
+        except:
+            return jsonify({"message": "Неверный формат json"}), 415
+    # session["id"] = db.get_users(data["email"])
+    else:
+        return jsonify({"message": "Неверный формат данных"}), 415
+
+
+@app.route("/verification", methods=["POST"])
+def token_verification():
+    if request.is_json:
+        # try:
+        db = DB("react_db")
+        data = request.get_json()
+        print(data)
+        data = jwt.decode(data["token"], app.config["SECRET_KEY"], algorithms=['HS256'])
+        print(data)
+        if db.select_by("id", data["id"]) is not None:
+            if int(data["exp"]) <= time.time():
+                return jsonify({"message": "время сеанса истекло"}), 401
+            else:
+                print("все ок")
+                return jsonify({"message": "Пользователь авторизован"}), 200
+        else:
+            return jsonify({"message": "пользователь не найден"}), 409
+
+        # except:
+        #     return jsonify({"message": "Неверный формат json"}), 415
+    else:
+        print("Неверный формат данных")
+        return jsonify({"message": "Неверный формат данных"}), 415
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
